08eb21bd343b00c910cb01a73d883b2651f2dae1
Gülsüm Tekkeşin <gulsum.tekkesin@gmail.com>
2026-06-07 15:35:48 +0300

    dialogue polishing

--- scripts/park_cutscene.gd
+++ scripts/park_cutscene.gd
@@ -4,7 +4,14 @@ extends Node2D
 @onready var soul = $Soul
 @onready var body = $Node2D/Body
 
+const MY_BALLOON = preload("res://dialogues/balloon.tscn")
 
+func _show_dialogue(resource: DialogueResource, title: String) -> void:
+	var balloon = MY_BALLOON.instantiate()
+	add_child(balloon)
+	balloon.start(resource, title)
+	await balloon.tree_exited
+	
 func _ready():
 	animation_player.play("swinging")
 	animation_player.animation_finished.connect(_on_animation_finished)
@@ -13,11 +20,13 @@ func _on_animation_finished(anim):
 	print("Animasyon bitti: ", anim)
 	match anim:
 		"swinging":
-			#DialogueManager.show_dialogue_balloon(
-				#preload("res://dialogues/bus_cutscene.dialogue"), "stop1"
-			#)
+			await _show_dialogue( 
+				preload("res://dialogues/park.dialogue"), "second")
 			animation_player.play("soul_run")
+			_show_dialogue(
+				preload("res://dialogues/park.dialogue"), "third")
 		"soul_run":
+				
 			# park_cutscene.gd'de soul_run bitince:
 				Global.is_soul_mode = false
 				Global.entrance_name ="park_bridge"
